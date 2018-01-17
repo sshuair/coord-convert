4c1fa8a5d09ba0ec640984ab2d57c774fba78be0
sshuair <sshuair@gmail.com>
2018-01-17 16:10:26 +0800

    support point multipoint polyline multipolyline polygon multipolygon

--- coordconvert/coordconvert.py
+++ coordconvert/coordconvert.py
@@ -12,6 +12,8 @@ from utils import Transform
 
 logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 
+def recur_map(f, data):
+    return[ not type(x) is list and f(x) or recur_map(f, x) for x in data ]
 
 def point_convert(collections, convert_type):
     transform = Transform()
@@ -20,71 +22,64 @@ def point_convert(collections, convert_type):
         fea['geometry']['coordinates'] = getattr(transform, convert_type)(coords[0], coords[1])
     return collections
 
-def line_convert(collections, convert_type):
-    pass
+def linestring_convert(collections, convert_type):
+    transform = Transform()
+    for fea in collections:
+        line_rings = fea['geometry']['coordinates']
+        fea['geometry']['coordinates'] = list(map(lambda x: getattr(transform, convert_type)(x[0], x[1]), line_rings))
+    return collections
+
+def polygon_convert(collections, convert_type):
+    transform = Transform()
+    for fea in collections:
+        line_rings = fea['geometry']['coordinates']
+        fea['geometry']['coordinates'] = list(map(lambda x: getattr(transform, convert_type)(x[0], x[1]), line_rings))
+    return collections
+
 
 def converter(in_path, out_path, convert_type):
     with fiona.open(in_path, 'r', encoding='utf-8') as source:
         source_schema = source.schema.copy()
         with fiona.open(out_path, 'w', encoding='utf-8', **source.meta) as target:
-            assert source_schema['geometry'] in ('Point', 'LineString', 'Polygon')
-            if source_schema['geometry'] == 'Point':
-                converted_collection = point_convert(list(source), convert_type)
-                for fea in tqdm(converted_collection):
-                    target.write(fea)
+            
+            transform = Transform()
+            f = lambda x: getattr(transform, convert_type)(x[0], x[1])
+            for fea in tqdm(source):
+                collections = fea['geometry']['coordinates']
+                if type(collections) is tuple:
+                    fea['geometry']['coordinates'] = f(collections)
+                elif type(collections) is list:
+                    fea['geometry']['coordinates'] = recur_map(f, collections)
+                else:
+                    raise TypeError("collection must be list or tuple")
+                target.write(fea)
+            # return collections
+            
+            # for f
 
-            elif source_schema['geometry'] == 'LineString':
-                for f in source:
-                    coords = f['geometry']['coordinates']
-                    pass
+            # assert source_schema['geometry'] in ('Point', 'LineString', 'Polygon')
+
+            # if source_schema['geometry'] == 'Point':
+            #     converted_collection = point_convert(list(source), convert_type)
+            #     for fea in tqdm(converted_collection):
+            #         target.write(fea)
+            # elif source_schema['geometry'] == 'LineString':
+            #     converted_collection = linestring_convert(list(source), convert_type)
+            #     for fea in tqdm(converted_collection):
+            #         target.write(fea)
+            # elif source_schema['geometry'] == 'Polygon':
+            #     converted_collection = polygon_convert(list(source), convert_type)
+            #     for fea in tqdm(converted_collection):
+            #         target.write(fea)
             # elif source_schema['geometry'] == 'Polygon':
             #     for f in source:
             #         f['geometry']['coordinates']
 
 point_path = './test/data/point/point.shp'
-# line_path = './test/data/line/polyline.shp'
+line_path = './test/data/line/polyline.shp'
+poylygon_path = './test/data/polygon/polygon-2.shp'
 out_path = './test/data/point_gcj.shp'
 converter(point_path, out_path, 'wgs2gcj')
+converter(line_path, out_path, 'wgs2gcj')
+converter(poylygon_path, out_path, 'wgs2gcj')
 
-
-
-# def signed_area(coords):
-#     """Return the signed area enclosed by a ring using the linear time
-#     algorithm at http://www.cgafaq.info/wiki/Polygon_Area. A value >= 0
-#     indicates a counter-clockwise oriented ring.
-#     """
-#     xs, ys = map(list, zip(*coords))
-#     xs.append(xs[1])
-#     ys.append(ys[1])
-#     return sum(xs[i]*(ys[i+1]-ys[i-1]) for i in range(1, len(coords)))/2.0
-
-# with fiona.open('./test/data/point/point.shp', 'r') as source:
-
-#     # Copy the source schema and add two new properties.
-#     sink_schema = source.schema.copy()
-#     sink_schema['properties']['s_area'] = 'float'
-#     sink_schema['properties']['timestamp'] = 'datetime'
-
-#     # Create a sink for processed features with the same format and
-#     # coordinate reference system as the source.
-#     with fiona.open('oriented-ccw.shp', 'w',crs=source.crs, driver=source.driver,schema=sink_schema,) as sink:
-#         for f in source:
-#             g = f['geometry']
-#             assert g['type'] == "Polygon"
-#             rings = g['coordinates']
-#             sa = sum(signed_area(r) for r in rings)
-#             if sa < 0.0:
-#                 rings = [r[::-1] for r in rings]
-#                 g['coordinates'] = rings
-#                 f['geometry'] = g
-
-#             # Add the signed area of the polygon and a timestamp
-#             # to the feature properties map.
-#             f['properties'].update(
-#                 s_area=sa,
-#                 timestamp=datetime.datetime.now().isoformat() )
-
-#             sink.write(f)
-
-
-        # The sink file is written to disk and closed when its block ends.
